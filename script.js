626a81ea5991a01ce354fe507b23401d37d2eb33
maurya512 <patelmaurya0512@gmail.com>
2020-07-24 23:32:55 -0400

    ask the user for prompts and then create variables to store those prompts

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var specialCase = "!@#$%^&*()_+[]{}|/~`".split("");
 
+// ask the user for prompts and store them inside variables 
+var passChar = prompt("How many characters do you want in your password?");
+var wantLower = confirm("Do you want lower case characters in your password?");
+var wantUpper = confirm("Do you wanr upper case characters in your password?");
+var wantNum = confirm("Do you want numbers in your password?");
+var wantSpc = confirm("Do you want special characters in your password?");
+
