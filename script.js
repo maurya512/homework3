1062565ad6a98a14ed6b9b6715982f3f161ccf49
maurya512 <patelmaurya0512@gmail.com>
2020-07-25 01:12:58 -0400

    checked if the user selected at least 1 criteria

--- script.js
+++ script.js
@@ -1,54 +1,86 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-var numbers = "1234567890".split("");
-var lettersLower = "abcdefghijklmnopqrstuvwxyz".split("");
-var lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
-var specialChar = "!#$%&*+<=>?@/[]^_`|~".split("");
-// User Prompts
-var howManyChar = prompt("How many characters would you like your password to be?");
-var userLowerLetters = confirm("Press OK if you want lowercase letters in your password.");
-var userUpperLetters = confirm("Press OK if you want uppercase letters in your password.")
-var userNumbers = confirm("Press OK if you want numbers in your password.");
-var userSpecialChar = confirm("Press OK if you want special characters in your password.");
-function generatePassword(howMany, letLow, letUp, numb, char) {
-  var myArray = [];
+
+// create variables to store password categories 
+var numbers = "0123456789".split("");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var specialCase = "!@#$%^&*()_+[]{}|/~`".split("");
+
+// ask the user for prompts and store them inside variables 
+var passChar = prompt("How many characters do you want in your password?");
+var wantLower = confirm("Do you want lower case characters in your password?");
+var wantUpper = confirm("Do you wanr upper case characters in your password?");
+var wantNum = confirm("Do you want numbers in your password?");
+var wantSpc = confirm("Do you want special characters in your password?");
+
+// create a function that takes the user prompts as arguments
+function generatePassword(totalChar, passLow, passUpper, passNum, passSpc) {
+  // create an empty array that will be populated once the user makes a selection of the type of password they want i.e lower, upper, number, etc. 
+  var passArr = [];
+
+  // create an empty array that will be populated with randomized characters that will eventually become the user's password 
   var result = [];
-  if (howMany >= 8 && howMany <= 128) {
-    if (letLow === true) {
-      for (var i = 0; i < lettersLower.length; i++) {
-        myArray.push(lettersLower[i]);
+
+  // make sure that user atleast selects one condition
+  if (totalChar === false && passLow === false && passUpper === false && passNum == false && passSpc === false) {
+    // check 1 of the password conditions of the length being between 8 and 128
+    if (totalChar >= 8 && totalChar <= 128) {
+      // if the user wants lower case characters then 
+      if (passLow === true) {
+        // for each character in the lowerCase variable "push" it into the passArr
+        for (var i = 0; i < lowerCase.length; i++) {
+          passArr.push(lowerCase[i]);
+        }
+      }
+      // if the user wants upper case characters then 
+      if (passUpper === true) {
+        // for each character in the upperCase var "push" them into the passArr
+        for (var i = 0; i < upperCase.length; i++) {
+          passArr.push(upperCase[i]);
+        }
+      }
+      // if the user wants numericals then
+      if (passNum === true) {
+        // for each character in the numbers var "push" them into the passArr
+        for (var i = 0; i < numbers.length; i++) {
+          passArr.push(numbers[i]);
+        }
+      }
+      // if the user wants special characters then
+      if (passSpc === true) {
+        // for each character in the specialCase var "push" them into the passArr
+        for (var i = 0; i < specialCase.length; i++) {
+          passArr.push(specialCase[i]);
+        }
+      }
+
+      // push the randomized characters from passArr into result array, passChar number of times 
+      for (var i = 0; i < passChar; i++) {
+        var randomNum = Math.floor(Math.random() * passArr.length);
+        result.push(passArr[randomNum]);
       }
     }
-    if (letUp === true) {
-      for (var i = 0; i < lettersUpper.length; i++) {
-        myArray.push(lettersUpper[i]);
-      }
+
+    // if the user enters less than 8 or more than 128 characters as preference then the system should reprompt them to enter a valid number of characters
+    else {
+      alert("Please select between 8 and 128 characters!");
+      return location.reload()
     }
-    if (numb === true) {
-      for (var i = 0; i < numbers.length; i++) {
-        myArray.push(numbers[i]);
-      }
-    }
-    if (char === true) {
-      for (var i = 0; i < specialChar.length; i++) {
-        myArray.push(specialChar[i]);
-      }
-    }
-    for (var i = 0; i < howMany; i++) {
-      var randomNum = Math.floor(Math.random() * myArray.length);
-      result.push(myArray[randomNum]);
-    }
-  } else {
-    alert("Please enter between 8 and 128 characters!")
+  }
+  else {
+    alert("Atleast select 1 criteria for the password!");
     return location.reload()
   }
+  // joins the individual characters into the array as a "string"
   return result.join("");
 }
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(howManyChar, userLowerLetters, userUpperLetters, userNumbers, userSpecialChar);
+  var password = generatePassword(passChar, wantLower, wantUpper, wantNum, wantSpc);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
