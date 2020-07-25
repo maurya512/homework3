6b97469b5f9c088f7189624cae00db63cd459586
maurya512 <patelmaurya0512@gmail.com>
2020-07-25 17:26:06 -0400

    updated the user criteria verifuing that the user enters a number as password's length

--- script.js
+++ script.js
@@ -22,53 +22,57 @@ function generatePassword(totalChar, passLow, passUpper, passNum, passSpc) {
   var result = [];
 
   // make sure that user atleast selects one condition
-  if (totalChar === false && passLow === false && passUpper === false && passNum == false && passSpc === false) {
-    // check 1 of the password conditions of the length being between 8 and 128
-    if (totalChar >= 8 && totalChar <= 128 && totalChar != null) {
-      // if the user wants lower case characters then 
-      if (passLow === true) {
-        // for each character in the lowerCase variable "push" it into the passArr
-        for (var i = 0; i < lowerCase.length; i++) {
-          passArr.push(lowerCase[i]);
-        }
-      }
-      // if the user wants upper case characters then 
-      if (passUpper === true) {
-        // for each character in the upperCase var "push" them into the passArr
-        for (var i = 0; i < upperCase.length; i++) {
-          passArr.push(upperCase[i]);
-        }
-      }
-      // if the user wants numericals then
-      if (passNum === true) {
-        // for each character in the numbers var "push" them into the passArr
-        for (var i = 0; i < numbers.length; i++) {
-          passArr.push(numbers[i]);
-        }
-      }
-      // if the user wants special characters then
-      if (passSpc === true) {
-        // for each character in the specialCase var "push" them into the passArr
-        for (var i = 0; i < specialCase.length; i++) {
-          passArr.push(specialCase[i]);
-        }
-      }
+  if (passLow === false && passUpper === false && passNum === false && passSpc === false) {
+    alert("Please select atleast 1 criteria for the password!");
+    return location.reload();
+  }
 
-      // push the randomized characters from passArr into result array, passChar number of times 
-      for (var i = 0; i < passChar; i++) {
-        var randomNum = Math.floor(Math.random() * passArr.length);
-        result.push(passArr[randomNum]);
+  // make sure that user enters a numerical value for the length of the password
+  if (totalChar === "" || totalChar === null || isNaN(totalChar)) {
+    alert("Please enter numerical value for the password's length!");
+    return location.reload();
+  }
+  // check 1 of the password conditions of the length being between 8 and 128
+  if (parseInt(totalChar) >= 8 && parseInt(totalChar) <= 128) {
+    // if the user wants lower case characters then 
+    if (passLow === true) {
+      // for each character in the lowerCase variable "push" it into the passArr
+      for (var i = 0; i < lowerCase.length; i++) {
+        passArr.push(lowerCase[i]);
+      }
+    }
+    // if the user wants upper case characters then 
+    if (passUpper === true) {
+      // for each character in the upperCase var "push" them into the passArr
+      for (var i = 0; i < upperCase.length; i++) {
+        passArr.push(upperCase[i]);
+      }
+    }
+    // if the user wants numericals then
+    if (passNum === true) {
+      // for each character in the numbers var "push" them into the passArr
+      for (var i = 0; i < numbers.length; i++) {
+        passArr.push(numbers[i]);
+      }
+    }
+    // if the user wants special characters then
+    if (passSpc === true) {
+      // for each character in the specialCase var "push" them into the passArr
+      for (var i = 0; i < specialCase.length; i++) {
+        passArr.push(specialCase[i]);
       }
     }
 
-    // if the user enters less than 8 or more than 128 characters as preference then the system should reprompt them to enter a valid number of characters
-    else {
-      alert("Please select between 8 and 128 characters!");
-      return location.reload()
+    // push the randomized characters from passArr into result array, passChar number of times 
+    for (var i = 0; i < passChar; i++) {
+      var randomNum = Math.floor(Math.random() * passArr.length);
+      result.push(passArr[randomNum]);
     }
   }
+
+  // if the user enters less than 8 or more than 128 characters as preference then the system should reprompt them to enter a valid number of characters
   else {
-    alert("Atleast select 1 criteria for the password!");
+    alert("Please select between 8 and 128 characters!");
     return location.reload()
   }
   // joins the individual characters into the array as a "string"
